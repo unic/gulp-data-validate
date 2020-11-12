dba0ed081489a9c04f03884898653e16e3f815a2
Robin Löffel <robin.loeffel@unic.com>
2020-11-12 14:21:02 +0100

    adapt to new .editorconfig rules

--- source/index.js
+++ source/index.js
@@ -11,60 +11,60 @@ const $refParser = require('json-schema-ref-parser');
 const pluginName = 'gulp-data-validate';
 
 module.exports = ({
-    schemaSuffix = '.schema.json',
-    failOnError = false,
-    ignorePrefix = '_'
+  schemaSuffix = '.schema.json',
+  failOnError = false,
+  ignorePrefix = '_'
 } = {}) => {
-    const stream = new Transform({ objectMode: true });
+  const stream = new Transform({ objectMode: true });
 
-    stream._transform = async (file, enc, done) => {
-        const ajv = new Ajv({ allErrors: true });
-        const schemaPath = file.path.replace('.data.js', schemaSuffix);
-        const fileName = path.basename(file.path);
+  stream._transform = async (file, _encoding, done) => {
+    const ajv = new Ajv({ allErrors: true });
+    const schemaPath = file.path.replace('.data.js', schemaSuffix);
+    const fileName = path.basename(file.path);
 
-        if (!fs.existsSync(schemaPath)) {
-            if (fileName.indexOf(ignorePrefix) === 0) {
-                done(null, file);
-                return;
-            }
+    if (!fs.existsSync(schemaPath)) {
+      if (fileName.indexOf(ignorePrefix) === 0) {
+        done(null, file);
+        return;
+      }
 
-            fancyLog(`${chalk.cyan(pluginName)}: couldn't find a schema for ${chalk.yellow(file.relative)}, skipping it`);
-            done(null, file);
-            return;
-        }
+      fancyLog(`${chalk.cyan(pluginName)}: couldn't find a schema for ${chalk.yellow(file.relative)}, skipping it`);
+      done(null, file);
+      return;
+    }
 
-        try {
-            const schemaData = importFresh(schemaPath);
-            const moduleData = importFresh(file.path);
-            const dereferencedSchemaData = await $refParser.dereference(schemaPath, schemaData, {});
-            const validate = ajv.compile(dereferencedSchemaData);
-            const moduleVariants = Object.values(moduleData.variants).map(v => v.props);
+    try {
+      const schemaData = importFresh(schemaPath);
+      const moduleData = importFresh(file.path);
+      const dereferencedSchemaData = await $refParser.dereference(schemaPath, schemaData, {});
+      const validate = ajv.compile(dereferencedSchemaData);
+      const moduleVariants = Object.values(moduleData.variants).map(v => v.props);
 
-            moduleVariants.forEach((variant, idx) => {
-                const valid = validate(variant);
+      moduleVariants.forEach((variant, idx) => {
+        const valid = validate(variant);
 
-                if (!valid) {
-                    const name = Object.keys(moduleData.variants)[idx];
+        if (!valid) {
+          const name = Object.keys(moduleData.variants)[idx];
 
-                    validate.errors.forEach(error => {
-                        fancyLog(`${chalk.cyan(pluginName)}: ${chalk.red(error.message)} ${chalk.red(JSON.stringify(error.params))} in variant ${chalk.yellow(name)} in ${chalk.yellow(file.relative)}`);
-                    });
+          validate.errors.forEach(error => {
+            fancyLog(`${chalk.cyan(pluginName)}: ${chalk.red(error.message)} ${chalk.red(JSON.stringify(error.params))} in variant ${chalk.yellow(name)} in ${chalk.yellow(file.relative)}`);
+          });
 
-                    if (failOnError) {
-                        stream.emit('error', new PluginError(pluginName, `${file.relative} isn't valid!`, {
-                            fileName: file.path
-                        }));
-                    }
-                }
-            });
-
-            done(null, file);
-        } catch (error) {
-            stream.emit('error', new PluginError(pluginName, error, {
-                fileName: file.path
+          if (failOnError) {
+            stream.emit('error', new PluginError(pluginName, `${file.relative} isn't valid!`, {
+              fileName: file.path
             }));
+          }
         }
-    };
+      });
 
-    return stream;
+      done(null, file);
+    } catch (error) {
+      stream.emit('error', new PluginError(pluginName, error, {
+        fileName: file.path
+      }));
+    }
+  };
+
+  return stream;
 };
