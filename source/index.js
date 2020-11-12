98814e4e0ee1a8afb829d31f60c3160f0f2d9943
Robin Löffel <robin.loeffel@unic.com>
2020-11-12 14:48:52 +0100

    only exit with `failOnError` after every variant has been validated

--- source/index.js
+++ source/index.js
@@ -1,7 +1,7 @@
 const { Transform } = require('stream');
 const fs = require('fs');
 const path = require('path');
-const importFresh = require('import-fresh');
+const fresh = require('import-fresh');
 const Ajv = require('ajv');
 const PluginError = require('plugin-error');
 const chalk = require('chalk');
@@ -14,57 +14,54 @@ module.exports = ({
   schemaSuffix = '.schema.json',
   failOnError = false,
   ignorePrefix = '_'
-} = {}) => {
-  const stream = new Transform({ objectMode: true });
-
-  stream._transform = async (file, _encoding, done) => {
+} = {}) => new Transform({
+  objectMode: true,
+  async transform(file, _encoding, done) {
     const ajv = new Ajv({ allErrors: true });
     const schemaPath = file.path.replace('.data.js', schemaSuffix);
     const fileName = path.basename(file.path);
 
-    if (!fs.existsSync(schemaPath)) {
-      if (fileName.indexOf(ignorePrefix) === 0) {
-        done(null, file);
-        return;
-      }
+    if (fileName.startsWith(ignorePrefix)) {
+      return done(null, file);
+    }
 
+    if (!fs.existsSync(schemaPath)) {
       fancyLog(`${chalk.cyan(pluginName)}: couldn't find a schema for ${chalk.yellow(file.relative)}, skipping it`);
-      done(null, file);
-      return;
+      return done(null, file);
     }
 
     try {
-      const schemaData = importFresh(schemaPath);
-      const moduleData = importFresh(file.path);
+      const schemaData = fresh(schemaPath);
+      const moduleData = fresh(file.path);
       const dereferencedSchemaData = await $refParser.dereference(schemaPath, schemaData, {});
       const validate = ajv.compile(dereferencedSchemaData);
       const moduleVariants = Object.values(moduleData.variants).map(v => v.props);
 
-      moduleVariants.forEach((variant, idx) => {
-        const valid = validate(variant);
+      let variantIsValid = false;
 
-        if (!valid) {
-          const name = Object.keys(moduleData.variants)[idx];
+      moduleVariants.forEach((variant, index) => {
+        variantIsValid = validate(variant);
+
+        if (!variantIsValid) {
+          const name = Object.keys(moduleData.variants)[index];
 
           validate.errors.forEach(error => {
             fancyLog(`${chalk.cyan(pluginName)}: ${chalk.red(error.message)} ${chalk.red(JSON.stringify(error.params))} in variant ${chalk.yellow(name)} in ${chalk.yellow(file.relative)}`);
           });
-
-          if (failOnError) {
-            stream.emit('error', new PluginError(pluginName, `${file.relative} isn't valid!`, {
-              fileName: file.path
-            }));
-          }
         }
       });
 
-      done(null, file);
+      if (failOnError && !variantIsValid) {
+        return this.emit('error', new PluginError(pluginName, `${file.relative} isn't valid!`, {
+          fileName: file.path
+        }));
+      }
+
+      return done(null, file);
     } catch (error) {
-      stream.emit('error', new PluginError(pluginName, error, {
+      return this.emit('error', new PluginError(pluginName, error, {
         fileName: file.path
       }));
     }
-  };
-
-  return stream;
-};
+  }
+});
